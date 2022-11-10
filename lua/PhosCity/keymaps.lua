2aefc2ac299725bcf29dbbb9fb6a0df3442bbd46
PhosCity <minatouzumaki8613@gmail.com>
2022-11-10 17:07:03 +0545

    keymaps to resize with arrows

--- lua/PhosCity/keymaps.lua
+++ lua/PhosCity/keymaps.lua
@@ -1,2 +1,8 @@
 lvim.keys.normal_mode["<S-l>"] = ":bnext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":bprevious<CR>"
+
+-- Resize with arrows
+lvim.keys.normal_mode["<C-Down>"] = ":resize -2<CR>"
+lvim.keys.normal_mode["<C-Up>"] = ":resize +2<CR>"
+lvim.keys.normal_mode["<C-Right>"] = ":vertical resize -2<CR>"
+lvim.keys.normal_mode["<C-Left>"] = ":vertical resize +2<CR>"
