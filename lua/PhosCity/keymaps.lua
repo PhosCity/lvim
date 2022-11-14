88a733315d98dfce2b5460530ad2bda901c17e98
PhosCity <minatouzumaki8613@gmail.com>
2022-11-14 17:19:49 +0545

    select al

--- lua/PhosCity/keymaps.lua
+++ lua/PhosCity/keymaps.lua
@@ -6,3 +6,6 @@ lvim.keys.normal_mode["<C-Down>"] = ":resize -2<CR>"
 lvim.keys.normal_mode["<C-Up>"] = ":resize +2<CR>"
 lvim.keys.normal_mode["<C-Right>"] = ":vertical resize -2<CR>"
 lvim.keys.normal_mode["<C-Left>"] = ":vertical resize +2<CR>"
+
+-- Select all
+lvim.keys.normal_mode["<C-a>"] = "gg<S-v>G"
