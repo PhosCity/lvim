d7f30c8e98896c986d4610d9f69860f83e4d55b4
PhosCity <minatouzumaki8613@gmail.com>
2022-11-16 08:59:30 +0545

    lazy load formatters and linters

--- lua/PhosCity/lsp.lua
+++ lua/PhosCity/lsp.lua
@@ -16,25 +16,3 @@ lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enable = true
 lvim.builtin.treesitter.rainbow.enable = true
 
--- Formatters
-local formatters = require("lvim.lsp.null-ls.formatters")
-formatters.setup({
-	{
-		command = "prettier",
-		extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-		filetypes = { "java" },
-	},
-	{ command = "stylua", filetypes = { "lua" } },
-	{ command = "shfmt", extra_args = { "-ci", "-i", "0" }, filetype = { "sh" } },
-	{ command = "black", extra_args = { "--fast" }, filetypes = { "python" } },
-})
-
--- Linters
-local linters = require("lvim.lsp.null-ls.linters")
-linters.setup({
-	{ command = "flake8", args = { "--ignore", "E501" } },
-	{
-		command = "shellcheck",
-		args = { "-o", "all", "-e", "2312" },
-	},
-})
