2b4e564366ab1c8b4b2886d87eb919b4689e5770
PhosCity <minatouzumaki8613@gmail.com>
2022-11-18 17:47:49 +0545

    update bool toggle func

--- lua/PhosCity/utils.lua
+++ lua/PhosCity/utils.lua
@@ -6,8 +6,12 @@ local M = {}
 function M.toggle_bool(args)
 	if args.word == "true" then
 		vim.cmd([[norm ciwfalse]])
+	elseif args.word == "True" then
+		vim.cmd([[norm ciwFalse]])
 	elseif args.word == "false" then
 		vim.cmd([[norm ciwtrue]])
+	elseif args.word == "False" then
+		vim.cmd([[norm ciwTrue]])
 	else
 		print("Word under cursor needs to be 'true' or 'false")
 	end
